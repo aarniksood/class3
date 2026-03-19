35ba1a04671080a44bfcb0261432c0b4f3cdc6bf
aarniksood <aarniksood@gmail.com>
2026-03-19 20:35:01 +0530

    first commit

---
+++ activity2.py
@@ -0,0 +1,12 @@
+name = "penguin"
+age = 15
+is_student = True
+weight = 38.5
+
+print("\n After Type casting.....")
+age = str(age)
+print(age)
+print("data Type of age is", type(age))
+weight = int(weight)
+print(weight)
+print("data type of weight is", type(weight))
---
+++ activity3.py
@@ -0,0 +1,6 @@
+text = str(input("enter a string:"))
+
+revtext = text[::-1]
+text = revtext
+print("reverse of the given string is :")
+print(text)
